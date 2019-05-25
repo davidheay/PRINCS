bb2650bbc6cd785d8c81039f8225ed0111cc7140
mariasaavega <mariasaavega@hotmail.com>
2019-05-25 15:15:37 -0500

    clases

--- src/main/java/app/champion/PRINCS/modelo/Lote.java
+++ src/main/java/app/champion/PRINCS/modelo/Lote.java
@@ -10,5 +10,52 @@ package app.champion.PRINCS.modelo;
  * @author Usuario
  */
 public class Lote {
+    String idLote;
+    String NombreL;
+    String Capacidad;
+    String Lugar;
+    
+        public Lote(String idLote, String NombreL) {
+    this.idLote = idLote;
+    this.NombreL = NombreL;
+}
+    
+    
+    public Lote() {
+    
+}
+       public String getIDLote() {
+        return idLote;
+    }
+            public void setIdLote(String idLote) {
+        this.idLote = idLote;
+    }
+
+       
+       public String getNombreL() {
+           return NombreL;
+       }
+               public void setNombreL(String NombreL) {
+        this.NombreL = NombreL;
+    }
+               public String getCapacidad() {
+        return Capacidad;
+    }
+            public void setCapacidad(String Capacidad) {
+        this.Capacidad = Capacidad;
+    }
+
+       
+       public String getLugar() {
+           return Lugar;
+       }
+               public void setLugar(String Lugar) {
+        this.Lugar = Lugar;
+    }
+       @Override
+    public String toString() {
+        return "Lote{" + "idLote=" + idLote + ", Nombre=" + NombreL + ", Capacidad=" + Capacidad + ", Lugar=" + Lugar + '}';
+    }
+    
     
 }
